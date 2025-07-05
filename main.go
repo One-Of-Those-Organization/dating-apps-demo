bda16c71f194199418527cc6572daa66d468bafd
Fernando Perry <fernandoperry1234@gmail.com>
2025-07-05 16:03:55 +0700

    chore: move stuff around

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	l "log"
-
-	// "github.com/gofiber/fiber/v2"
-	// "gorm.io/gorm"
-	// "gorm.io/driver/sqlite"
-)
-
 func main() {
-	const dbFile = "databse.sqlite"
-	db, err := ReadDB(dbFile)
-	if err != nil {
-		l.Printf("Failed to opent the db, %v.", err)
-		return
-	}
-	err = MigrateDB(db)
-	if err != nil {
-		l.Printf("Failed to migrate the db, %v.", err)
-		return
-	}
+	_ = init_server()
 }
--- server.go
+++ server.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	l "log"
+
+	// "github.com/gofiber/fiber/v2"
+	// "gorm.io/gorm"
+	// "gorm.io/driver/sqlite"
+)
+
+func init_server() error {
+	const dbFile = "db/databse.sqlite"
+	db, err := ReadDB(dbFile)
+	if err != nil {
+		l.Printf("Failed to opent the db, %v.", err)
+		return err
+	}
+	err = MigrateDB(db)
+	if err != nil {
+		l.Printf("Failed to migrate the db, %v.", err)
+		return err
+	}
+	return nil
+}
